511fdf9cb8f049ab2ee6e262d819f7c72d4464ac
nicholasgriffen <nicholas.s.griffen@gmail.com>
2018-11-30 14:36:15 -0700

    complete date formatting

--- src/utility/DateLib.js
+++ src/utility/DateLib.js
@@ -2,7 +2,7 @@
     FormatDateString is used in Post.js. It takes a Date,
     performs various calculations based on the current date, 
     and returns a different string depending on whether the post
-    was created < 10 seconds, < one minute, yesterday, today, or more than 1 day ago  
+    was created yesterday, < 10 seconds, < one minute, today, or more than 1 day ago  
 */
 export function FormatDateString(date) {
     const secondsAgo = ((Date.now() - date.getTime()) / 1000)
@@ -12,13 +12,14 @@ export function FormatDateString(date) {
     //getDate returns a number representing the calendar day of the date
     const createdYesterday = (new Date()).getDate() - 1 === date.getDate()
 
-    if (secondsAgo < 10) {
+    // check yesterday first, it could be yesterday with a difference of 1 second at 12AM
+    if (createdYesterday) {
+        return `Yesterday at ${hours}:${minutes}${AMPM}`
+    } else if (secondsAgo < 10) {
         return `a few seconds ago`
     } else if (secondsAgo < 60) {
         return `${parseInt(secondsAgo, 10)} seconds ago`
-    } else if (createdYesterday) {
-        return `Yesterday at ${hours}:${minutes}${AMPM}`
-    } else if (secondsAgo < (24 * 3600)) {
+    }  else if (secondsAgo < (24 * 3600)) {
         return `${hours}:${minutes}${AMPM}`
     } else {
         //date is more than 1 day old, return a string with mm/dd/yyyy
@@ -36,6 +37,8 @@ export function padSingleDigits(num) {
 
 // convertHours takes a date and converts the hours from 24-hour format to 12-hour format 
 export function convertHours(date) {
-    const militaryHours = date.getHours() + 1
-    return militaryHours > 12 ? militaryHours - 12 : militaryHours
+    const militaryHours = date.getHours()
+    return militaryHours === 0 ? 12 // midnight
+        : militaryHours > 12 ? militaryHours - 12 
+        : militaryHours 
 }
